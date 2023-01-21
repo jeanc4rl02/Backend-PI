d8721667d5c95dc03b14de33086be0f5a55a2a9c
Damián Cano López <damiancano1811@hotmail.com>
2023-01-21 18:31:45 -0500

    Implementacion de Lombok, y Tests

--- booking/src/main/java/com/booking/booking/entities/Entrenador.java
+++ booking/src/main/java/com/booking/booking/entities/Entrenador.java
@@ -1,8 +1,16 @@
 package com.booking.booking.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "categorias")
 public class Entrenador {
     @Id
@@ -15,52 +23,9 @@ public class Entrenador {
     @Column
     private String url;
 
-    public Entrenador(Long id, String nombre, String descripcion, String url) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.url = url;
-    }
-
     public Entrenador(String nombre, String descripcion, String url) {
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.url = url;
-    }
-
-    public Entrenador() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
         this.url = url;
     }
 }
